5e50c3274303a8e56884085c3a40f2548fbe1652
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-17 17:40:51 +0200

    added blank div and cosmetics changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,19 @@
 
 const Header = () => {
   return (
-    <section className='p-3 border-[#283044] border-b-2 border-solid'>
-      <div className="flex flex-row items-center justify-between gap-2 text-white">
-        <div className="">
-            <button>
-            <div className="bg-[#1d2133] rounded-full p-1">
-                <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+    <section className='p-3 border-[#2B344D] border-b-[1px]'>
+      <div className="flex flex-row items-center justify-between  gap-2 text-white">
+        <div className="flex justify-center items-center">
+          <button>
+            <div className="bg-[#2B344D] rounded-full p-[6px] ">
+              <svg className=" text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h14M5 12l4-4m-4 4 4 4" />
-                </svg>
+              </svg>
             </div>
-            </button>
+          </button>
         </div>
-        <div className="text-lg font-semibold">Select a token</div>
-        <div className=""> </div>
+        <div className="text-[16px] leading-[22px] font-bold">Select a token</div>
+        <div className="w-[32px] h-[32px]"></div>
       </div>
     </section>
   )
