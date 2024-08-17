f0e13ba2c02b436cc9e61925730ebb7dad704092
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-17 17:43:24 +0200

    added search icon and styles to input

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
 
 const SearchInput = () => {
-    const [state, setState] = useState('');
-  return (
-    <section className='py-2'>
-        <div className="">
-            <input className='w-full' placeholder='Search' >
-            
-            </input>
-            <button>x</button>
-        </div>
-    </section>
-  )
+
+    return (
+        <section className='py-2'>
+            <div className="relative">
+                <input
+                    className='w-full px-[8px] py-[9px] rounded-[6px] border-[#2B344D] border-[1px] bg-[#040407] text-[#7685A0]' placeholder='Search'
+                />
+                <div className="">
+                    <button className='absolute right-[10px] top-[12px]'>
+                        <svg
+                            className="w-[20px] h-[20px] text-gray-800 dark:text-[#7685A0]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-width="2" d="m21 21-3.5-3.5M17 10a7 7 0 1 1-14 0 7 7 0 0 1 14 0Z"
+                            />
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export default SearchInput
