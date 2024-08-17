35cfd44739deba83850151a8fb4311c0fd1dd852
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-17 11:31:54 +0200

    added components to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
-
+import Header from "./components/Header"
+import SearchInput from "./components/SearchInput"
+import AppWrapper from "./components/AppWrapper"
 
 function App() {
 
-
   return (
-    <div className="text-3xl">test</div>
+    <AppWrapper>
+      <main className="border-solid border-[#283044] border-2 rounded-xl">
+        <Header/>
+        <SearchInput/>
+        <div className="text-3xl">test</div>
+      </main>
+    </AppWrapper>
   )
 }
 
