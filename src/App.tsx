4e6d7c71325d14264c0a54f97348afd9c5ad49b8
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-17 17:39:43 +0200

    added components to main app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import Header from "./components/Header"
 import SearchInput from "./components/SearchInput"
 import AppWrapper from "./components/AppWrapper"
+import CryptocurrencyTile from "./components/CryptocurrencyTile"
 
 function App() {
 
   return (
     <AppWrapper>
-      <main className="border-solid border-[#283044] border-2 rounded-xl">
-        <Header/>
-        <SearchInput/>
-        <div className="text-3xl">test</div>
+      <main className="">
+        <Header />
+        <div className="p-3">
+          <SearchInput />
+          <CryptocurrencyTile percentage={2} />
+        </div>
       </main>
     </AppWrapper>
   )
