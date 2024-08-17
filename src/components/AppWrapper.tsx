4b0863e79628077473ec183913f4d4f390c04c60
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-17 17:40:08 +0200

    added more classes to app wrapper

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,9 +1,9 @@
 import { ReactNode } from 'react'
 
-const AppWrapper = ({children} : { children: ReactNode}) => {
+const AppWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='h-full mx-auto max-w-xl py-10'>
-        {children}
+    <div className='overflow-hidden max-h-[544px] max-w-[360px] mx-auto my-20 border-[#283044] border-[1px] rounded-xl shadow-custom'>
+      {children}
     </div>
   )
 }
