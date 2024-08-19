b10b0bcc3461777a91ab3ef9dbb091be8e294365
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-19 07:34:48 +0200

    added styles, and types to cryptocurrency tile

--- src/components/CryptocurrencyTile.tsx
+++ src/components/CryptocurrencyTile.tsx
@@ -1,34 +1,37 @@
-// { tokenFullName, tokenShortName, icons, price, logo, percentage }
-import nearImage from '../assets/near.png';
 import binanceImage from '../assets/binance.png';
 
 
-const CryptocurrencyTile = ({ percentage }: { percentage: number }) => {
-    const colorBoxClass = percentage > 0 ? 'text-[#70D1A3] bg-[#5AB88B]/25' : 'text-[#F08282] bg-[#D25858]/25';
-    const formattedPercentage = percentage > 0 ? `+${percentage}` : `${percentage}`;
+const CryptocurrencyTile = ({
+    icon, name, ticker, amount, percentChange
+}: {
+    icon: string, name: string, ticker: string, amount: number, percentChange: number
+}) => {
+    const colorBoxClass = percentChange >= 0 ? 'text-[#70D1A3] bg-[#5AB88B]/25' : 'text-[#F08282] bg-[#D25858]/25';
+    const formattedPercentage = percentChange > 0 ? `+${percentChange}` : `${percentChange}`;
 
     return (
         <div className=''>
-            <div className="bg-[#171C2F] px-2 py-1 rounded-[12px] mb-[4px]">
+            <div className="bg-[#171C2F] px-[12px] py-[8px] rounded-[12px] mb-[4px]">
                 <div className="flex justify-between">
-                    <div className="flex flex-row gap-2">
+                    <div className="flex flex-row gap-2 justify-center items-center">
                         <div className="relative">
-                            <img className="w-[48px] h-[48px]" src={nearImage} alt="Near Logo" />
-                            <div className="absolute bottom-0.5 -right-4 flex ">
-                                <img className="w-[12px] h-[12px] -mr-[50%]" src={binanceImage} alt="Near Logo" />
-                                <img className="w-[12px] h-[12px] -mr-[50%]" src={binanceImage} alt="Near Logo" />
-                                <img className="w-[12px] h-[12px] -mr-[50%]" src={binanceImage} alt="Near Logo" />
-
+                            <img className="w-[48px] h-[48px]" src={icon} alt="" />
+                            <div className="absolute bottom-0.5 right-2 -space-x-1.5 flex ">
+                                <img className="w-[12px] h-[12px] " src={binanceImage} alt="Near Logo" />
+                                <img className="w-[12px] h-[12px] " src={binanceImage} alt="Near Logo" />
+                                <img className="w-[12px] h-[12px] " src={binanceImage} alt="Near Logo" />
                             </div>
                         </div>
-                        <div className="">
-                            <h2>DogeCoin</h2>
-                            <p>Doge</p>
+                        <div className="flex flex-col items-start justify-between gap-[4px]">
+                            <h2 className='font-bold text-[16px]'>{name}</h2>
+                            <p className='font-normal text-[10px] py-[2px] px-[5px] bg-[#3E4864] rounded-[4px]'>{ticker}</p>
                         </div>
                     </div>
-                    <div className="flex flex-col text-end">
-                        <div className="font-bold text-[16px]">$1.00</div>
-                        <div className={`p-[1px] rounded-[4px] ${colorBoxClass}`}>
+                    <div className="flex flex-col items-end gap-[4px]">
+                        <div className="font-bold text-[16px]">${amount}</div>
+                        <div className={`
+                        px-[8px] py-[2px] text-[12px] font-normal rounded-[4px] w-[56px] text-center ${colorBoxClass}
+                        `}>
                             {formattedPercentage}%
                         </div>
                     </div>
