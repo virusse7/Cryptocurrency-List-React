dd809b5c2c172e9f9904c718a958ec9bc770358e
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-19 15:53:48 +0200

    changed component to list item and added small icons with math random

--- src/components/CryptocurrencyTile.tsx
+++ src/components/CryptocurrencyTile.tsx
@@ -1,4 +1,5 @@
 import binanceImage from '../assets/binance.png';
+import nearImage from '../assets/near.png'
 
 
 const CryptocurrencyTile = ({
@@ -7,19 +8,32 @@ const CryptocurrencyTile = ({
     icon: string, name: string, ticker: string, amount: number, percentChange: number
 }) => {
     const colorBoxClass = percentChange >= 0 ? 'text-[#70D1A3] bg-[#5AB88B]/25' : 'text-[#F08282] bg-[#D25858]/25';
-    const formattedPercentage = percentChange > 0 ? `+${percentChange}` : `${percentChange}`;
+    const formattedPercentage = percentChange > 0 ? `+${percentChange.toFixed(2)}` : `${percentChange.toFixed(2)}`;
+    const numIcons = Math.floor(Math.random() * 10) + 1;
+    const iconsToShow = numIcons > 3 ? 3 : numIcons;
+    const extraIconsCount = numIcons - iconsToShow;
+
+    const smallIcons = [nearImage, binanceImage,nearImage, binanceImage,nearImage, binanceImage,nearImage, binanceImage]
 
     return (
-        <div className=''>
+        <li className=''>
             <div className="bg-[#171C2F] px-[12px] py-[8px] rounded-[12px] mb-[4px]">
                 <div className="flex justify-between">
                     <div className="flex flex-row gap-2 justify-center items-center">
                         <div className="relative">
-                            <img className="w-[48px] h-[48px]" src={icon} alt="" />
-                            <div className="absolute bottom-0.5 right-2 -space-x-1.5 flex ">
-                                <img className="w-[12px] h-[12px] " src={binanceImage} alt="Near Logo" />
-                                <img className="w-[12px] h-[12px] " src={binanceImage} alt="Near Logo" />
-                                <img className="w-[12px] h-[12px] " src={binanceImage} alt="Near Logo" />
+                            <img className="w-[48px] h-[48px]" src={nearImage} alt="" />
+                            <div className="absolute bottom-0.5 left-4 -space-x-1.5 flex ">
+                                {Array.from({length: iconsToShow}).map((_, index) => {
+                                    const randomIcon = smallIcons[Math.floor(Math.random() * smallIcons.length)];
+                                    return (
+                                        <img key={index} className='w-[12px] h-[12px]' src={randomIcon} alt='small icon' />
+                                    ) 
+                                })}
+                                {extraIconsCount > 0 && (
+                                    <div className="w-[12px] h-[12px] bg-gray-500 text-[6px] font-bold rounded-full flex items-center justify-center">
+                                        {extraIconsCount}+
+                                    </div>
+                                )}
                             </div>
                         </div>
                         <div className="flex flex-col items-start justify-between gap-[4px]">
@@ -37,7 +51,7 @@ const CryptocurrencyTile = ({
                     </div>
                 </div>
             </div>
-        </div>
+        </li>
     )
 }
 
