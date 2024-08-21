7d9de3908889e02dd74cae49ee8d039b420fda76
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-21 11:24:49 +0200

    added function to generate small icons

--- src/components/generateData.tsx
+++ src/components/generateData.tsx
@@ -5,7 +5,9 @@ interface Cryptocurrency {
     "ticker": string;
     "amount": number;
     "percentChange": number;
+    "smallIcons" : string[];
 }
+
 function generateRandomAmount() {
     const min = 0;
     const max = 1_000_000;
@@ -21,19 +23,34 @@ function generateRandomPercentage() {
     return parseFloat(randomPercentage.toFixed(2));
 }
 
+function getRandomSmallIcons(baseData: Cryptocurrency[], count: number): string[] {
+    const smallIcons: string[] = [];
+    const totalIcons = Math.min(count, baseData.length)
+
+    for (let i = 0; i < totalIcons ;i++) {
+        const randomIndex = Math.floor(Math.random() * baseData.length);
+        smallIcons.push(baseData[randomIndex].icon)
+    }
+
+    return smallIcons;
+}
+
 const generateData = (baseData: Cryptocurrency[], count: number) => {
     const maxRecords = 10_000;
     const validCount = Math.min(count, maxRecords);
-
+    
     const result: Cryptocurrency[] = [];
     for (let i = 0; i < validCount; i++) {
         const randomCryptocurrency = baseData[i % baseData.length];
+        const smallIconsCount = Math.floor((Math.random() * 7) + 4)
+        const smallIcons = getRandomSmallIcons(baseData, smallIconsCount)
         result.push({
             icon: randomCryptocurrency.icon,
             name: randomCryptocurrency.name,
             ticker: randomCryptocurrency.ticker,
             amount: generateRandomAmount(),
-            percentChange: generateRandomPercentage()
+            percentChange: generateRandomPercentage(),
+            smallIcons: smallIcons
         })
     }
 
