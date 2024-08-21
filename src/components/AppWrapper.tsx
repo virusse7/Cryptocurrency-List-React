7d611f57796dbad1868de23fa3bc99a3d3bf1dcc
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-08-21 11:24:07 +0200

    changed margin

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react'
 
 const AppWrapper = ({ children }: { children: ReactNode }) => {
   return (
-    <div className='overflow-y-scroll max-h-[544px] max-w-[360px] mx-auto mt-20 border-[#283044] border-[1px] rounded-xl shadow-custom'>
+    <div className='overflow-y-scroll max-h-[544px] max-w-[360px] mx-auto mt-10 border-[#283044] border-[1px] rounded-xl shadow-custom'>
       {children}
     </div>
   )
