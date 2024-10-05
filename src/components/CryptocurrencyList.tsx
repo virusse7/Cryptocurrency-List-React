b009fd49cad08c9aae6c47584f2b8b7709c20b88
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-10-05 13:22:00 +0200

    added infinite scrolling and padding to prevent of being covered by scroll

--- src/components/CryptocurrencyList.tsx
+++ src/components/CryptocurrencyList.tsx
@@ -1,8 +1,9 @@
-import { useMemo, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import baseData from '../data.json';
 import CryptocurrencyTile from './CryptocurrencyTile';
 import generateData from './generateData';
 import SearchInput from './SearchInput';
+import InfiniteScroll from 'react-infinite-scroller';
 
 interface CryptocurrencyListProps {
     recordCount: number;
@@ -10,6 +11,7 @@ interface CryptocurrencyListProps {
 
 const CryptocurrencyList: React.FC<CryptocurrencyListProps> = ({ recordCount }) => {
     const [searchTerm, setSearchTerm] = useState<string>('');
+    const [itemsToShow, setItemsToShow] = useState<number>(100);
 
     const generatedData = useMemo(() => {
         return generateData(baseData, recordCount)
@@ -24,26 +26,49 @@ const CryptocurrencyList: React.FC<CryptocurrencyListProps> = ({ recordCount }) 
 
     const handleSearch = (searchedText: string) => {
         setSearchTerm(searchedText)
+        setItemsToShow(100);
     }
+
+    const loadMore = useCallback(() => {
+        setTimeout(() => {
+            setItemsToShow((prev) => {
+                const newItemsToShow = prev + 100;
+                return newItemsToShow;
+            });
+        }, 1000);
+    }, []);
+
+    const hasMoreItems = itemsToShow < filteredData.length;
 
     return (
         <>
             <SearchInput
                 onSearch={handleSearch}
             />
-            <ul className="">
-                {filteredData.map((crypto, index) => (
-                    <CryptocurrencyTile
-                        key={index}
-                        amount={crypto.amount}
-                        icon={crypto.icon}
-                        name={crypto.name}
-                        percentChange={crypto.percentChange}
-                        ticker={crypto.ticker}
-                        smallIcons={crypto.smallIcons}
-                    />
-                ))}
-            </ul>
+            <div className="overflow-y-auto h-[400px]">
+                <InfiniteScroll
+                    pageStart={0}
+                    loader={<h4 key={0} className='text-center pt-2'>Loading...</h4>}
+                    hasMore={hasMoreItems}
+                    loadMore={loadMore}
+                    useWindow={false}
+                >
+                    <ul className="pr-2">
+                        {filteredData.slice(0, itemsToShow).map((crypto, index) => (
+                            <CryptocurrencyTile
+                                key={index}
+                                amount={crypto.amount}
+                                icon={crypto.icon}
+                                name={crypto.name}
+                                percentChange={crypto.percentChange}
+                                ticker={crypto.ticker}
+                                smallIcons={crypto.smallIcons}
+                            />
+                        ))}
+                    </ul>
+                </InfiniteScroll>
+            </div>
+
         </>
     )
 }
