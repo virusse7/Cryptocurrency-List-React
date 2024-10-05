138f0baf22410fb0f5eb263e8cbb42db76b0ea39
Daniel Wieczorek <danielwieczorek901@gmail.com>
2024-10-05 13:17:41 +0200

    changed import

--- src/components/AppWrapper.tsx
+++ src/components/AppWrapper.tsx
@@ -1,11 +1,11 @@
-import { ReactNode } from 'react'
+import React from "react";
 
-const AppWrapper = ({ children }: { children: ReactNode }) => {
-  return (
-    <div className='overflow-y-scroll max-h-[544px] max-w-[360px] mx-auto mt-10 border-[#283044] border-[1px] rounded-xl shadow-custom'>
-      {children}
-    </div>
-  )
+const AppWrapper = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className='overflow-y-scroll max-h-[544px] max-w-[360px] mx-auto mt-10 border-[#283044] border-[1px] rounded-xl shadow-custom'>
+            {children}
+        </div>
+    )
 }
 
 export default AppWrapper
